0f1f432e670d8ba44eaa92315c7e3c9408ab3dfb
jay <jaylong255@gmail.com>
2017-05-10 13:18:04 -0500

    debugging the master branch

--- frontend_avatar_upload.php
+++ frontend_avatar_upload.php
@@ -3,7 +3,7 @@
  * Plugin Name: Frontend Avatar Upload
  * Plugin URI: ''
  * Description: Plugin to place a avatar and change avatar of a currently  logged in user in a wordpress frontend by using a shortcode
- * Version: 0.9
+ * Version: 0.9.2
  * Author: Jose Alfonso del Callar
  * Author URI:
  * Text Domain:
@@ -19,7 +19,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
- 
+
 spl_autoload_register( 'frontend_avatar_upload_autoload' );
 
 function frontend_avatar_upload_autoload($class) {
@@ -39,9 +39,10 @@ function frontend_avatar_upload_autoload($class) {
     }
 }
 
+
 define('FRONTEND_AVATAR_UPLOAD_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 define('FRONTEND_AVATAR_UPLOAD_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
 define('FRONTEND_AVATAR_UPLOAD_PLUGIN_FILE',  'frontend_avatar_upload.php');
 define('FRONTEND_AVATAR_UPLOAD_DEV_MODE', true);
 
-new FrontendAvatarUpload\Frontend_Avatar_Upload;
+new FrontendAvatarUpload\\Frontend_Avatar_Upload;
